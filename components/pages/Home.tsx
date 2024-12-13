818c2e62d5e387b19ea28de22f6463b816adf7a6
Josh <josh.bondy@codekiwi.com>
2024-12-13 14:00:28 +1300

    update with preview

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -3,14 +3,14 @@ import { LoadingState } from "../shared/LoadingState";
 import ButtonConnect from "../shared/ButtonConnect";
 
 import { useAssets, useWallet } from "@meshsdk/react";
-import { CompileStatusEnum, toPreDefinedUnit, toUserDefinedUnit, useCompileCampaign } from "@jetplane/velocity-tools";
-import { useCallback, useEffect, useRef, useState } from "react"; 
+import { CompileStatusEnum, toPrecompileInputUnit, toPreDefinedUnit, toUserDefinedUnit, useCompileCampaign } from "@jetplane/velocity-tools";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Assets } from "../wallet/Assets";
 import { WaletAsset } from "../wallet/Asset";
 import useAsset from "../hooks/useAsset";
 import { Asset } from "@meshsdk/core";
- 
+
 enum Step {
     IMAGE,
     FRAME,
@@ -34,6 +34,7 @@ const Home = () => {
     const [step, setStep] = useState(Step.IMAGE);
     const [frames, setFrames] = useState<any[]>([]);
     const [quoteResponse, setQuoteResponse] = useState<any>(null);
+    const [previewImage, setPreviewImage] = useState<string | null>(null);
 
     const [myAssets, setMyAssets] = useState<any>(null);
     const { fetchAsset } = useAsset();
@@ -76,206 +77,210 @@ const Home = () => {
         }
     }, [step, imageInput, frameInput, pfpInput, captionInput]);
 
+    useEffect(() => {
+        if (quoteResponse?.quote?.preview) {
+            setPreviewImage(quoteResponse?.quote?.preview?.path?.split("/")?.pop());
+        }
+    }, [quoteResponse]);
 
-        
-
-
-  if (!connected && connecting) {
-    return (
-      <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
-        <Layout title="Connecting Wallet">
-          <div className="flex justify-center items-center">
-            <LoadingState />
-          </div>
-        </Layout>
-      </div>
-    );
-  }
-
-  if (!connected && !connecting) {
-    return (
-      <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
-        <Layout title="">
-          <div className="flex justify-center items-center">
-            <ButtonConnect />
-          </div>
-        </Layout>
-      </div>
-    );
-  }
-
-  if (!campaignConfig) {
-    return (
-      <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
-        <Layout title="Special Moments">
-          <div className="flex justify-center items-center">
-            <LoadingState />
-          </div>
-        </Layout>
-      </div>
-    );
-  } 
-    
-        if (step === Step.FRAME){
-            return (
-                <Layout title="Select a frame">
-                    <div className="grid grid-cols-12 gap-5 h-auto">
-                        {frames.map((frame, index) => (
-                            <div key={index} className="col-span-4">
-                                <Image src={frame.image?.downloadURL} width={200} height={200} alt={frame.name} />
-                                <button
-                                    onClick={() => {
-                                        setFrameInput(frame);
-                                        setStep(Step.PFP);
-                                    }}
-                                    className="btn btn-primary mt-2"
-                                >
-                                    {frame.name}
-                                </button>
-                            </div>
-                        ))}
+    if (!connected && connecting) {
+        return (
+            <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
+                <Layout title="Connecting Wallet">
+                    <div className="flex justify-center items-center">
+                        <LoadingState />
                     </div>
                 </Layout>
-            );}
-    
-            if (step === Step.IMAGE){
-              return (
-                  <Layout title="Upload an image">
-                      <div className="grid grid-cols-12 gap-5 h-auto">
-                          <div className="col-span-8">
-                              <input ref={inputRef} className="file-input file-input-ghost w-full max-w-xs" type="file" accept="image/*" />
-                              <button
-                                  onClick={() => {
-                                      if (campaignConfig) {
-                                          setUserDefinedInput("image", "postcard", {}, inputRef?.current?.files[0]).then((result) => {
-                                              setImageInput(result);
-                                              setStep(Step.FRAME);
-                                          });
-                                      }
-                                  }}
-                                  className="btn btn-primary mt-2"
-                              >
-                                  Upload
-                              </button>
-                          </div>
-                          <div className="col-span-4"></div>
-                      </div>
-                  </Layout>
-              );}
-    
-        if (step === Step.PFP){
-            return (
-                <Layout title="Select a PFP">
-                    <div className="grid grid-cols-12 gap-5">
-                        {(myAssets || []).map(({ onchain_metadata: item, unit }: { onchain_metadata: any; unit: string }, index: any) => (
-                            <WaletAsset
-                                item={item}
-                                key={index}
-                                action={{
-                                    action: (item: any) => {
-                                        setPfpInput({ ...item, unit });
-                                        setStep(Step.CAPTION);
-                                        return null;
-                                    },
-                                    status,
-                                    label: () => "Select",
-                                }}
-                            />
-                        ))}
+            </div>
+        );
+    }
+
+    if (!connected && !connecting) {
+        return (
+            <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
+                <Layout title="">
+                    <div className="flex justify-center items-center">
+                        <ButtonConnect />
                     </div>
                 </Layout>
-            );}
-    
+            </div>
+        );
+    }
 
-    
-        if (step === Step.CAPTION){
-            return (
-                <Layout title="Enter a caption">
-                    <div className="grid grid-cols-12 gap-5 h-auto">
-                        <div className="col-span-8">
-                            <input ref={captionInputRef} className="textarea textarea-bordered" placeholder="Enter a Caption" type="textarea" />
+    if (!campaignConfig) {
+        return (
+            <div className="w-full h-screen bg-gray-10  bg-teds bg-bottom ">
+                <Layout title="Special Moments">
+                    <div className="flex justify-center items-center">
+                        <LoadingState />
+                    </div>
+                </Layout>
+            </div>
+        );
+    }
+
+    if (step === Step.FRAME) {
+        return (
+            <Layout title="Select a frame">
+                <div className="grid grid-cols-12 gap-5 h-auto">
+                    {frames.map((frame, index) => (
+                        <div key={index} className="col-span-4">
+                            <Image src={frame.image?.downloadURL} width={200} height={200} alt={frame.name} />
                             <button
                                 onClick={() => {
-                                    if (campaignConfig) {
-                                        setUserDefinedInput("caption", "postcard", captionInputRef?.current?.value).then((result) => {
-                                            setCaptionInput(result);
-                                            setStep(Step.REVIEW);
-                                        });
-                                    }
+                                    setFrameInput(frame);
+                                    setStep(Step.PFP);
                                 }}
                                 className="btn btn-primary mt-2"
                             >
-                                Review
+                                {frame.name}
                             </button>
                         </div>
-                        <div className="col-span-4"></div>
-                    </div>
-                </Layout>
-            );}
-    
+                    ))}
+                </div>
+            </Layout>
+        );
+    }
 
-    
-        if (step === Step.REVIEW){
-            return (
-                <Layout title="Enter a caption">
-                    <div className="grid grid-cols-12 gap-5 h-auto">
-                        <div className="col-span-8">
-                            {quoteResponse && (
-                                <div className="grid grid-cols-12 gap-5">
-                                    <div className="col-span-8">
-                                        {
-                                            // TODO: Display preview image
-                                        }
-                                        <Image src={quoteResponse?.quote?.preview} width={400} height={400} alt="Preview" />
-                                    </div>
-                                    <div className="col-span-4">
-                                        <div className="card">
-                                            <div className="card-body">
-                                                <h2 className="card-title">Mint Quote</h2>
-                                                <p>Price: {quoteResponse?.quote?.fee}A</p>
-                                            </div>
+    if (step === Step.IMAGE) {
+        return (
+            <Layout title="Upload an image">
+                <div className="grid grid-cols-12 gap-5 h-auto">
+                    <div className="col-span-8">
+                        <input ref={inputRef} className="file-input file-input-ghost w-full max-w-xs" type="file" accept="image/*" />
+                        <button
+                            onClick={() => {
+                                if (campaignConfig) {
+                                    setUserDefinedInput("image", "postcard", {}, inputRef?.current?.files[0]).then((result) => {
+                                        setImageInput(result);
+                                        setStep(Step.FRAME);
+                                    });
+                                }
+                            }}
+                            className="btn btn-primary mt-2"
+                        >
+                            Upload
+                        </button>
+                    </div>
+                    <div className="col-span-4"></div>
+                </div>
+            </Layout>
+        );
+    }
+
+    if (step === Step.PFP) {
+        return (
+            <Layout title="Select a PFP">
+                <div className="grid grid-cols-12 gap-5">
+                    {(myAssets || []).map(({ onchain_metadata: item, unit }: { onchain_metadata: any; unit: string }, index: any) => (
+                        <WaletAsset
+                            item={item}
+                            key={index}
+                            action={{
+                                action: (item: any) => {
+                                    setPfpInput({ ...item, unit });
+                                    setStep(Step.CAPTION);
+                                    return null;
+                                },
+                                status,
+                                label: () => "Select",
+                            }}
+                        />
+                    ))}
+                </div>
+            </Layout>
+        );
+    }
+
+    if (step === Step.CAPTION) {
+        return (
+            <Layout title="Enter a caption">
+                <div className="grid grid-cols-12 gap-5 h-auto">
+                    <div className="col-span-8">
+                        <input ref={captionInputRef} className="textarea textarea-bordered" placeholder="Enter a Caption" type="textarea" />
+                        <button
+                            onClick={() => {
+                                if (campaignConfig) {
+                                    setUserDefinedInput("caption", "postcard", captionInputRef?.current?.value).then((result) => {
+                                        setCaptionInput(result);
+                                        setStep(Step.REVIEW);
+                                    });
+                                }
+                            }}
+                            className="btn btn-primary mt-2"
+                        >
+                            Review
+                        </button>
+                    </div>
+                    <div className="col-span-4"></div>
+                </div>
+            </Layout>
+        );
+    }
+
+    if (step === Step.REVIEW) {
+        return (
+            <Layout title="Enter a caption">
+                <div className="grid grid-cols-12 gap-5 h-auto">
+                    <div className="col-span-8">
+                        {quoteResponse && (
+                            <div className="grid grid-cols-12 gap-5">
+                                <div className="col-span-8">
+                                    {
+                                        // TODO: Display preview image
+                                    }
+                                    <Image src={quoteResponse?.quote?.preview?.downloadURL} width={400} height={400} alt="Preview" />
+                                </div>
+                                <div className="col-span-4">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <h2 className="card-title">Mint Quote</h2>
+                                            <p>Price: {quoteResponse?.quote?.fee}A</p>
                                         </div>
                                     </div>
                                 </div>
-                            )}
-                            <button
-                                onClick={() => {
-                                    if (quoteResponse) {
-                                        compile("postcard", [{ unit: toUserDefinedUnit(imageInput?.id, "image") }, { unit: toUserDefinedUnit(captionInput?.id, "caption") }, { unit: toPreDefinedUnit(frameInput?.id, "frames") }, pfpInput]);
-                                    }
-                                }}
-                                className="btn btn-primary mt-2"
-                            >
-                                Mint
-                            </button>
-                        </div>
-                        <div className="col-span-4"></div>
+                            </div>
+                        )}
+                        <button
+                            onClick={() => {
+                                if (quoteResponse) {
+                                    compile("postcard", [
+                                        { unit: toUserDefinedUnit(imageInput?.id, "image") },
+                                        { unit: toUserDefinedUnit(captionInput?.id, "caption") },
+                                        { unit: toPreDefinedUnit(frameInput?.id, "frames") },
+                                        pfpInput,
+                                        toPrecompileInputUnit(campaignConfig.id, previewImage ?? ""),
+                                    ]);
+                                }
+                            }}
+                            className="btn btn-primary mt-2"
+                        >
+                            Mint
+                        </button>
                     </div>
-                </Layout>
-            );}
-    
-
-          }
+                    <div className="col-span-4"></div>
+                </div>
+            </Layout>
+        );
+    }
+};
 export default Home;
 
 export async function getStaticProps() {
-  /* Fetch data here */
-  const requestHeaders: HeadersInit = new Headers();
+    /* Fetch data here */
+    const requestHeaders: HeadersInit = new Headers();
 
-  requestHeaders.set(
-    "jetplane-api-key",
-    process.env.NEXT_PUBLIC_VELOCITY_API_KEY ?? ""
-  );
-  const res = await fetch(`${process.env.NEXT_PUBLIC_VELOCITY_API}/summary`, {
-    method: "GET",
-    headers: requestHeaders,
-  });
-  const summary = await res.json();
+    requestHeaders.set("jetplane-api-key", process.env.NEXT_PUBLIC_VELOCITY_API_KEY ?? "");
+    const res = await fetch(`${process.env.NEXT_PUBLIC_VELOCITY_API}/summary`, {
+        method: "GET",
+        headers: requestHeaders,
+    });
+    const summary = await res.json();
 
-  return {
-    props: {
-      summary,
-    },
-    revalidate: 5 * 60,
-  };
+    return {
+        props: {
+            summary,
+        },
+        revalidate: 5 * 60,
+    };
 }
