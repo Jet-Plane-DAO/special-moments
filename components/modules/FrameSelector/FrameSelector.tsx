f9d0024fdfed67c4f616388efd55d38532a118d6
Hardi <hardi@codekiwi.com>
2024-12-22 12:20:53 +0800

    fix: new selection way

--- components/modules/FrameSelector/FrameSelector.tsx
+++ components/modules/FrameSelector/FrameSelector.tsx
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousel from "../../shared/Carousel/Carousel";
-import { FrameSelectorTypes } from "./FrameSelectorTypes";
+// import { FrameSelectorTypes } from "./FrameSelectorTypes";
 import Image from "next/image";
+import { SelectedMask } from "@app/components/shared";
 
+interface FrameSelectorTypes {
+  onSelect: (x: any) => void;
+  frames?: Array<any>;
+  frame?: any;
+  className?: string;
+}
 export default function FrameSelector({
   onSelect,
   frames,
+  className = "",
 }: FrameSelectorTypes) {
+  const [selected, setSelected] = useState<any>(null);
   return (
     <div>
-      <div className="frame-carousel pb-10">
+      <div className={["frame-carousel pb-10", className].join(" ")}>
         <Carousel
           renderItem={(item) => {
             return (
-              <div className="">
-                <div className="relative pt-[100%]">
+              <button
+                onClick={() => {
+                  setSelected((_prev: any) => {
+                    if (!_prev) {
+                      onSelect(item);
+                      return item;
+                    }
+                    if (_prev?.id === item.id) {
+                      onSelect(null);
+                      return null;
+                    }
+                    onSelect(item);
+                    return item;
+                  });
+                }}
+                className="flex w-full h-full bg-transparent px-0 m-0 border-none"
+              >
+                <div className="relative frame-ratio w-full">
                   <Image
                     src={item.image?.downloadURL}
                     width="220"
                     height="220"
                     alt="frame"
-                    className="absolute object-scale-down w-full h-full top-0"
+                    className="absolute object-scale-down w-full h-full top-0 left-0"
                   />
                 </div>
-                <div
-                  className={`flex justify-center items-center pb-4 border-0 bg-transparent absolute top-0 w-full h-full `}
-                >
-                  <button  onClick={() => onSelect(item)} className="btn px-8">Select</button>
-                </div>
-              </div>
+                {selected?.id === item.id && <SelectedMask />}
+              </button>
             );
           }}
           slides={frames ?? []}
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,3 +2,5 @@ export { default as Button } from "./Button";
 export { default as Carousel } from "./Carousel/Carousel";
 export { default as ButtonHeader } from "./ButtonHeader/ButtonHeader";
 export type { ButtonHeaderProps } from "./ButtonHeader/ButtonHeader";
+export { default as FPFCard } from "./FPFCard/FPFCard";
+export { default as SelectedMask } from "./SelectedMask/SelectedMask";
