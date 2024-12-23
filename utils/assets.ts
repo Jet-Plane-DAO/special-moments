e38c14cdaa1eb13567ae6ed045530c1c3819e7f1
Hardi <hardi@codekiwi.com>
2024-12-23 17:30:28 +0800

    fix: image utils and update logo

--- utils/assets.ts
+++ utils/assets.ts
@@ -1,6 +1,6 @@
-export function getPublicImageURL(filename: string, cdn = false): string {
+export function getPublicImageURL(filename: string): string {
     if(!filename) {
         return '';
     }
-    return process.env.NEXT_PUBLIC_IMAGE_SOURCE_URL + filename;
+    return (process.env.NEXT_PUBLIC_IMAGE_SOURCE_URL ?? '/assets/images/fpo/') + filename;
 }
